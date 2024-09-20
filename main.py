f23c156eb9ae8158795650447a8ad6919dd680c1
zahorisbdj72 <129951552+zahorisbdj72@users.noreply.github.com>
2024-09-20 16:45:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import plate_detection
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
 
-directory = "Test/Frontal/Frontal/"
-for name in os.listdir(directory)[:-1]:
+from plate_detection import *
+
+directory = "Images/Test/Front/"
+for name in os.listdir(directory):
     contours = find_relevant_contours(directory+name)
     contours = find_extremes(contours)
     index, error = calculate_error(contours)
     print(error, len(contours))
-    img = cv2.cvtColor(cv2.imread(directory+name), cv2.COLOR_BGR2RGB)
-    cv2.drawContours(img, contours[index], -1, (255,0,0), 100)
-    plt.figure(figsize=(16, 16))
-    plt.imshow(img, cmap="gray")
+    img = cv2.imread(directory+name)
+    cv2.drawContours(img, contours[index], -1, (0,0,255), 100)
+    img = cv2.resize(img, (960, 540))
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
