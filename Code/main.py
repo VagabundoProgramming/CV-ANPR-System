7124172523a6db7104dd2b26fb28be39a09d2dec
zahorisbdj72 <129951552+zahorisbdj72@users.noreply.github.com>
2024-10-14 16:10:18 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -10,10 +10,14 @@ for name in os.listdir(directory):
     contours = find_relevant_contours(directory+name)
     contours = find_extremes(contours)
     index, error = calculate_error(contours)
-    print(error, len(contours))
     img = cv2.imread(directory+name)
-    cv2.drawContours(img, contours[index], -1, (0,0,255), 100)
-    img = cv2.resize(img, (960, 540))
+    
+    #cv2.drawContours(img, contours[index], -1, (0,0,255), 100)
+    
+    pts = order_points(contours[index])
+    img = four_point_transform(img, pts, custom_resolution=(520, 110))
+
+    print(template_matching(img, figures))
     cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
